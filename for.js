1f67f61d82b8bfad6bf9c3dab2e8e79b546115e1
devmahin <mahinhowlader123@gmail.com>
2024-01-29 21:41:40 +0600

    for even & for odd

--- for.js
+++ for.js
@@ -1,18 +1,38 @@
-const top10BooksBangladesh = [
-    "A Golden Age",
-    "The Good Muslim",
-    "The Hungry Tide",
-    "The Blood Telegram: Nixon, Kissinger, and a Forgotten Genocide",
-    "A Golden Age",
-    "River of Smoke",
-    "The Glass Palace",
-    "The Newlyweds",
-    "Brick Lane",
-    "Interpreter of Maladies"
-];
-let num = 1;
-for(const store of top10BooksBangladesh){
-    console.log(store)
-    console.log(num++)
-}
+// const top10BooksBangladesh = [
+//     "A Golden Age",
+//     "The Good Muslim",
+//     "The Hungry Tide",
+//     "The Blood Telegram: Nixon, Kissinger, and a Forgotten Genocide",
+//     "A Golden Age",
+//     "River of Smoke",
+//     "The Glass Palace",
+//     "The Newlyweds",
+//     "Brick Lane",
+//     "Interpreter of Maladies"
+// ];
+// let num = 1;
+// for(const store of top10BooksBangladesh){
+//     console.log(store)
+//     console.log(num++)
+// }
 
+
+
+
+
+
+
+// for(let i=0; i<20; i++){
+//     console.log(i)
+// }
+
+
+
+// even
+for(let i=0; i<=20; i += 2){
+    console.log(i)
+}
+// odd
+for(let i=1; i<=20; i += 2){
+    console.log(i)
+}
