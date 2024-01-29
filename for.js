47d7757d37381f52dc4a1c675b109db966c5f85a
devmahin <mahinhowlader123@gmail.com>
2024-01-29 19:14:54 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,18 @@
+const top10BooksBangladesh = [
+    "A Golden Age",
+    "The Good Muslim",
+    "The Hungry Tide",
+    "The Blood Telegram: Nixon, Kissinger, and a Forgotten Genocide",
+    "A Golden Age",
+    "River of Smoke",
+    "The Glass Palace",
+    "The Newlyweds",
+    "Brick Lane",
+    "Interpreter of Maladies"
+];
+let num = 1;
+for(const store of top10BooksBangladesh){
+    console.log(store)
+    console.log(num++)
+}
+
