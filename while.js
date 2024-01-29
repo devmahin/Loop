c3ab5c66e7b36fd7cbb8c92faf68345b85dccc12
devmahin <mahinhowlader123@gmail.com>
2024-01-29 21:37:06 +0600

    while

---
+++ while.js
@@ -0,0 +1,22 @@
+// let num = 1;
+// let sum = 1;
+
+// while(num       < 5){
+//     console.log("Repeted", num)
+//     num += 1;   
+//     console.log("sum" , sum)
+//     sum += num;
+// }
+
+
+// even while loop
+let numConvat = 1;
+while(numConvat <= 28){
+    console.log(numConvat)
+    if(numConvat % 3 === 1){
+        console.log("even",numConvat)
+    }
+    numConvat++;
+}
+
+
